a21c0e5a571f877f64f3336374cd24ca84855223
Nicolas Frankel <nicolas@frankel.ch>
2021-01-28 16:49:43 +0100

    Initial commit

---
+++ app/src/main/java/org/hazelcast/zerodowntime/entity/Product.java
@@ -0,0 +1,53 @@
+package org.hazelcast.zerodowntime.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Entity
+public class Product implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+
+    /** Mandated by JPA. */
+    protected Product() {}
+
+    Product(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
