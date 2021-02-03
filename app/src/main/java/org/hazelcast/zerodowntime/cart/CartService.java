1c7b907fa1890f32bd1009ce16cd6c9b7a82b37a
Nicolas Frankel <nicolas@frankel.ch>
2021-02-03 18:03:17 +0100

    Change the data model
      Introduce a new table that makes the model incompatible with the previous version of the application

--- app/src/main/java/org/hazelcast/zerodowntime/cart/CartService.java
+++ app/src/main/java/org/hazelcast/zerodowntime/cart/CartService.java
@@ -1,29 +1,38 @@
 package org.hazelcast.zerodowntime.cart;
 
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Objects;
+import java.util.Optional;
+import javax.servlet.http.HttpSession;
+import org.hazelcast.zerodowntime.customer.CustomerRepository;
 import org.hazelcast.zerodowntime.customer.CustomerView;
+import org.hazelcast.zerodowntime.entity.Cart;
 import org.hazelcast.zerodowntime.entity.CartLine;
-import org.hazelcast.zerodowntime.entity.CartLineId;
+import org.hazelcast.zerodowntime.entity.Customer;
 import org.hazelcast.zerodowntime.operation.OperationContext;
 import org.springframework.session.FindByIndexNameSessionRepository;
 
-import javax.servlet.http.HttpSession;
-import java.util.Collection;
-import java.util.Optional;
-
 public class CartService {
 
-    private final CartLinesRepository cartRepository;
+    private final CustomerRepository customerRepository;
+    private final CartRepository cartRepository;
     private final OperationContext context;
 
-    public CartService(CartLinesRepository cartRepository, OperationContext context) {
+    public CartService(CustomerRepository customerRepository,
+                       CartRepository cartRepository,
+                       OperationContext context) {
+        this.customerRepository = customerRepository;
         this.cartRepository = cartRepository;
         this.context = context;
     }
 
     public CartView view(HttpSession session) {
         var itemCount = getCustomerId(session).stream()
-                .map(cartRepository::findAllByCustomer)
-                .flatMap(Collection::stream)
+                .map(customerRepository::findByIdWithCartLines)
+                .map(Customer::getCart)
+                .filter(Objects::nonNull)
+                .flatMap(cart -> cart.getCartLines().stream())
                 .map(CartLine::getQuantity)
                 .mapToInt(quantity -> quantity)
                 .sum();
@@ -31,13 +40,39 @@ public class CartService {
     }
 
     public CartView add(HttpSession session, Long productId) {
-        var customerId = getCustomerId(session).orElseThrow();
-        var cartLineId = new CartLineId(customerId, productId);
-        var cartLine = cartRepository.findById(cartLineId)
-                .orElse(new CartLine(customerId, productId));
-        cartLine.incrementQuantity();
-        cartRepository.saveAndFlush(cartLine);
+        var customer = getCustomerId(session)
+                .stream()
+                .map(customerRepository::findByIdWithCartLines)
+                .findAny()
+                .orElseThrow();
+        var cart = customer.getCart();
+        if (cart == null) {
+            var newCart = new Cart();
+            customer.setCart(newCart);
+            cartRepository.saveAndFlush(newCart);
+            cart = newCart;
+            addToCart(cart, productId).run();
+        } else {
+            Optional.ofNullable(cart.getCartLines())
+                    .stream()
+                    .flatMap(Collection::stream)
+                    .filter(line -> line.getProduct().getId().equals(productId))
+                    .findAny()
+                    .ifPresentOrElse(
+                            CartLine::incrementQuantity,
+                            addToCart(cart, productId)
+                    );
+        }
+        cart.setLastModified(LocalDateTime.now());
+        customerRepository.saveAndFlush(customer);
         return view(session);
+    }
+
+    private Runnable addToCart(Cart cart, Long productId) {
+        return () -> {
+            var cartLine = new CartLine(cart, productId); // Won't probably work
+            cart.addCartLine(cartLine);
+        };
     }
 
     private Optional<Long> getCustomerId(HttpSession session) {
